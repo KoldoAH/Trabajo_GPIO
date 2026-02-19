7d0efd694645991ff1fe46449ad6f80a9eecde05
Svergara2525 <sergioverdom@gmail.com>
2026-02-19 23:57:10 +0100

    Modificar puerto API para ejecutar desde local

--- Backend/app.py
+++ Backend/app.py
@@ -62,4 +62,4 @@ def conexion():
         return jsonify({"mensaje": "Error al conectarse al endpoint", "error": str(e)}), 500
     
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5003)
